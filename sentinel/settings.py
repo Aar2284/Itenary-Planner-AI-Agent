8c6ac351843f885d465a33bfd2e2907bee966bc6
aaryan <aaryankalia165@gmail.com>
2026-07-31 23:57:53 +0530

    feat: Transitioning to Asynchronous Django

--- sentinel/settings.py
+++ sentinel/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pulse',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'sentinel.wsgi.application'
+ASGI_APPLICATION = 'sentinel.asgi.application'
 
 
 # Database
