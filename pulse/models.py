5bcd7837528335d57ec20319ba7eeaeb6e9edf33
aaryan <aaryankalia165@gmail.com>
2026-08-02 20:45:39 +0530

    feat: Add AnomalyEvent database model

--- pulse/models.py
+++ pulse/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class AnomalyEvent(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    sensor_reading = models.FloatField()
+    severity_score = models.IntegerField()
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"Anomaly at {self.timestamp} - {self.sensor_reading}°C"
