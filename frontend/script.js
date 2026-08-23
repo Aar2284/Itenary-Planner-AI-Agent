03d95904061a2e9118e55373d7e1fce814a83844
aaryan <aaryankalia165@gmail.com>
2026-08-23 18:59:07 +0530

    feat: Add session ID generation for unique users

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-let sessionId = 'session_' + Date.now();
+let sessionId = 'session_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
 
 async function sendMessage() {
     const input = document.getElementById('userInput');
